aa8a93cfda4c6e5ab749f538506ddf11994feeda
Sergiy-Galenko <131354874+Sergiy-Galenko@users.noreply.github.com>
2024-02-24 15:57:01 +0200

    update

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,24 @@
 (function ($) {
+
     "use strict";
-    $(window).load(function(){
-        $('.preloader').fadeOut(1000);
-    }) 
 
-    function initParallax() {
-        $('#home').parallax("100%", 0.3)
-        $('#about').parallax("20%", 0.3)
-        $('#work').parallax("40%", 0.3)
-        $('#contact').parallax("60%", 0.3)
-        $('#footer').parallax("80%", 0.3)
-    }
-    initParallax();
+        $(window).load(function(){
+          $('.preloader').fadeOut(1000);  
+        });
 
-    new WOW ({mobile: false}).init();
-})(jQuery)
 
+        // Parallax Js
+        function initParallax() {
+          $('#home').parallax("100%", 0.3);
+          $('#about').parallax("20%", 0.3);
+          $('#work').parallax("40%", 0.3);
+          $('#contact').parallax("60%", 0.3);
+          $('#footer').parallax("80%", 0.3);
+          }
+        initParallax(); 
+
+
+        // WOW Animation js
+        new WOW({ mobile: false }).init();
+
+})(jQuery);
